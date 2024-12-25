033b7a746ef36131c0ddc33233cc276b00c359d0
raghwerndra95124 <raghwerndrasingh95124@gmail.com>
2024-12-25 16:51:45 +0530

    test76886

---
+++ src/main/java/raghwendra12/test001.java
@@ -0,0 +1,13 @@
+package raghwendra12;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class test001 {
+    public static void main(String[] args) {
+        ChromiumDriver driver = new ChromeDriver();
+        driver.get("https://www.emedicoz.com/");
+
+    }
+}
