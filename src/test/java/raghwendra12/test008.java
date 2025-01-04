48c3171fad56aee88544e4f6b0ba655d97ad693c
raghwerndra95124 <raghwerndrasingh95124@gmail.com>
2025-01-04 12:48:39 +0530

    test76886

---
+++ src/test/java/raghwendra12/test008.java
@@ -0,0 +1,49 @@
+package raghwendra12;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test008 {
+    @Description("Verify that with invalid email, pass, error message is shown on the app.vwo.com")
+    @Test
+public void testemedicozlogin(){
+
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--incognito");
+        edgeOptions.addArguments("--start-maximized");
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.navigate().to("https://app.vwo.com");
+        System.out.println(driver.getTitle());
+
+        Assert.assertEquals(driver.getTitle(), "Login - VWO");
+        Assert.assertEquals(driver.getCurrentUrl(),"https://app.vwo.com/#/login");
+        WebElement emailInputBox = driver.findElement(By.id("login-username"));
+        emailInputBox.sendKeys("admin@admin.com");
+        WebElement passwordInputBox = driver.findElement(By.name("password"));
+        passwordInputBox.sendKeys("password@321");
+
+
+        WebElement buttonSubmit = driver.findElement(By.id("js-login-btn"));
+        buttonSubmit.click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        WebElement error_message = driver.findElement(By.className("notification-box-description"));
+        Assert.assertEquals(error_message.getText(),"Your email, password, IP address or location did not match");
+        driver.quit();
+
+
+
+    }
+
+}
